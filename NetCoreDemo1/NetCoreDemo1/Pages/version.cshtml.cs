1608823e9d63ea9aa5f8da7cafc7f0a8425243de
Adam Demos <adam.demos@outlook.com>
2021-03-31 13:12:53 +0200

    rename

---
+++ NetCoreDemo1/NetCoreDemo1/Pages/version.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Demo1.Pages
+{
+    public class VersionModel : PageModel
+    {
+        private readonly ILogger<VersionModel> _logger;
+
+        public VersionModel(ILogger<VersionModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
